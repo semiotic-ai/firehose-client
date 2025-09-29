5be886deb58b57c88dc697ae80b5ce1e868dce66
Joseph Livesey <joseph@semiotic.ai>
2025-09-29 13:20:19 -0400

    chore: remove circular dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -8,47 +8,48 @@
 //!
 //! ## Fetching an Ethereum Block
 //!
-//! ```rust,no_run
-//! # use firehose_client::{Chain, FirehoseClient};
-//! # use vee::EthBlock as Block;
-//! # #[tokio::main]
-//! # async fn main() -> Result<(), vee::ProtosError> {
-//! let mut client = FirehoseClient::new(Chain::Ethereum);
+//! ```rust,ignore
+//! use firehose_client::{Chain, FirehoseClient};
+//! use vee::EthBlock as Block;
 //!
-//! if let Some(response) = client.fetch_block(20672593).await.unwrap().ok() {
-//!     let block = Block::try_from(response.into_inner())?;
-//!     assert_eq!(block.number, 20672593);
-//!     assert_eq!(
-//!         format!("0x{}", hex::encode(block.hash)).as_str(),
-//!         "0xea48ba1c8e38ea586239e9c5ec62949ddd79404c6006c099bb02a8b22ddd18e4"
-//!     );
+//! #[tokio::main]
+//! async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     let mut client = FirehoseClient::new(Chain::Ethereum);
+//!
+//!     if let Some(response) = client.fetch_block(20672593).await?.ok() {
+//!         let block = Block::try_from(response.into_inner())?;
+//!         assert_eq!(block.number, 20672593);
+//!         assert_eq!(
+//!             format!("0x{}", hex::encode(block.hash)).as_str(),
+//!             "0xea48ba1c8e38ea586239e9c5ec62949ddd79404c6006c099bb02a8b22ddd18e4"
+//!         );
+//!     }
+//!     Ok(())
 //! }
-//! # Ok(())
-//! # }
 //! ```
 //!
 //! ## Streaming Ethereum Blocks
 //!
-//! ```rust,no_run
-//! # use firehose_client::{Chain, FirehoseClient};
-//! # use futures::StreamExt;
-//! # use vee::EthBlock as Block;
-//! # #[tokio::main]
-//! # async fn main() -> Result<(), vee::ProtosError> {
-//! const TOTAL_BLOCKS: u64 = 8192;
-//! const START_BLOCK: u64 = 19581798;
+//! ```rust,ignore
+//! use firehose_client::{Chain, FirehoseClient};
+//! use futures::StreamExt;
+//! use vee::EthBlock as Block;
 //!
-//! let mut client = FirehoseClient::new(Chain::Ethereum);
-//! let mut stream = client
-//!     .stream_blocks::<Block>(START_BLOCK, TOTAL_BLOCKS)
-//!     .await
-//!     .unwrap();
+//! #[tokio::main]
+//! async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     const TOTAL_BLOCKS: u64 = 8192;
+//!     const START_BLOCK: u64 = 19581798;
 //!
-//! while let Some(block) = stream.next().await {
-//!     // Do Something with the extracted stream of blocks.
+//!     let mut client = FirehoseClient::new(Chain::Ethereum);
+//!     let mut stream = client
+//!         .stream_blocks::<Block>(START_BLOCK, TOTAL_BLOCKS)
+//!         .await?;
+//!
+//!     while let Some(block) = stream.next().await {
+//!         // Do Something with the extracted stream of blocks.
+//!     }
+//!     Ok(())
 //! }
-//! # Ok(())
-//! # }
 //! ```
 //!
 
